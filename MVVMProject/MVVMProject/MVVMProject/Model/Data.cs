2aa8b11df5e297b48b428e9c2f69ea037c8de5c3
Unknown <chrispettinari@gmail.com>
2019-01-15 15:22:25 -0500

    Added the stuff for the thing
    haha yes

---
+++ MVVMProject/MVVMProject/MVVMProject/Model/Data.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVMProject.Model
+{
+    class Data
+    {
+        public class Result
+        {
+            public string title { get; set; }
+            public string href { get; set; }
+            public string ingredients { get; set; }
+            public string thumbnail { get; set; }
+        }
+
+        public class Recipe
+        {
+            public string title { get; set; }
+            public double version { get; set; }
+            public string href { get; set; }
+            public IList<Result> results { get; set; }
+
+
+
+
+
+            //foob
+        }
+    }
+}
---
+++ MVVMProject/MVVMProject/MVVMProject/ViewModel/RecipeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVMProject.ViewModel
+{
+    class RecipeViewModel
+    {
+    }
+}
